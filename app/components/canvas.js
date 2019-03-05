917685414b861f2319a9295dd5785a01083cc309
Spencer Price <spencer516@gmail.com>
2019-03-04 20:23:51 -0500

    [18. CREATE CANVAS SPRITE]

--- app/components/canvas.js
+++ app/components/canvas.js
@@ -1,7 +1,9 @@
 import Component from '@ember/component';
+import { set } from '@ember/object';
 import { inject as service } from '@ember-decorators/service';
 import { reads } from '@ember-decorators/object/computed';
 import { computed, action } from '@ember-decorators/object';
+import { Sprite } from '../utils/sprites';
 import { tagName } from '@ember-decorators/component';
 
 @tagName('')
@@ -25,11 +27,21 @@ export default class CanvasComponent extends Component {
 
   @reads('layerState.layers') layers;
 
-  @computed('{width,height}')
-  get backgroundStyles() {
-    return {
-      width: `${this.width}px`,
-      height: `${this.height}px`
-    };
+  @computed(
+    '{workspaceTranslation,workspaceZoom,workspaceRotation}',
+    '{width,height}'
+  )
+  get workspaceSprite() {
+    const { width: w, height: h } = this;
+    const {
+      workspaceTranslation: [offsetX, offsetY],
+      workspaceZoom: s,
+      workspaceRotation: r
+    } = this;
+
+    const x = offsetX - w / 2;
+    const y = offsetY - h / 2;
+
+    return Sprite.fromDimensions({ x, y, h, w, s, r });
   }
 }
