782e2f985ea5abc79a602019abffd06fa87c34b6
Spencer Price <spencer516@gmail.com>
2019-03-04 19:27:12 -0500

    [3. LAYER LIST SELECTION BONUS]

--- app/components/layer-list.js
+++ app/components/layer-list.js
@@ -4,16 +4,36 @@ import { action } from '@ember-decorators/object';
 import { reads } from '@ember-decorators/object/computed';
 import { tagName } from '@ember-decorators/component';
 
+const { max, min } = Math;
+
 @tagName('')
 export default class LayerListComponent extends Component {
   @service layerState;
   @service layerSelection;
 
+  prevLayerSelected = null;
+
   @reads('layerState.layers') layers;
   @reads('layerSelection.selectedLayers') selected;
 
   @action
-  selectLayer(layer, { metaKey }) {
-    this.layerSelection.selectLayer(layer, metaKey);
+  selectLayer(layer, { metaKey, shiftKey }) {
+    const prevLayer = this.prevLayerSelected;
+
+    if (shiftKey && prevLayer) {
+      const { layers } = this;
+      const prevIndex = layers.indexOf(prevLayer);
+      const curIndex = layers.indexOf(layer);
+      const startIndex = min(prevIndex, curIndex);
+      const endIndex = max(prevIndex, curIndex);
+
+      const layersToSelect = layers.slice(startIndex, endIndex + 1);
+
+      this.layerSelection.selectLayers(layersToSelect, true);
+    } else {
+      this.layerSelection.selectLayer(layer, metaKey);
+    }
+
+    this.prevLayerSelected = layer;
   }
 }
