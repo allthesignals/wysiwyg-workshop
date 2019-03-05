650bae10c5e22951a4e339eed9aedab5de13c554
Spencer Price <spencer516@gmail.com>
2019-03-04 19:51:17 -0500

    [8. DRAG TO DRAW BOX]

--- app/components/background-canvas.js
+++ app/components/background-canvas.js
@@ -1,15 +1,36 @@
 import Component from '@ember/component';
 import { tagName } from '@ember-decorators/component';
-import { computed } from '@ember-decorators/object';
+import { set } from '@ember/object';
+import { computed, action } from '@ember-decorators/object';
 
 @tagName('')
 export default class BackgroundCanvasComponent extends Component {
-  isDrawingBox = true;
-  startPosition = [100, 100];
-  currentOffset = [200, 200];
+  isDrawingBox = false;
+  startPosition = [0, 0];
+  currentOffset = [0, 0];
 
   height;
   width;
+
+  @action
+  backgroundDrag({ event, offset }) {
+    const { type } = event;
+
+    if (type === 'mousedown') {
+      const { offsetX, offsetY } = event;
+      set(this, 'isDrawingBox', true);
+      set(this, 'currentOffset', [0, 0]);
+      set(this, 'startPosition', [offsetX, offsetY]);
+      return;
+    }
+
+    if (type === 'mousemove') {
+      set(this, 'currentOffset', offset);
+      return;
+    }
+
+    set(this, 'isDrawingBox', false);
+  }
 
   @computed('{startPosition,currentOffset}')
   get boxStyles() {
