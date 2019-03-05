02510e1a84c555b0d4d629712efe0fe5b56fe756
Spencer Price <spencer516@gmail.com>
2019-03-04 19:38:28 -0500

    [5. INTERACTIVE LAYER KEYBOARD SELECTION SUPPORT]
    # Conflicts:
    #	app/templates/components/interactive-layer.hbs

--- app/components/interactive-layer.js
+++ app/components/interactive-layer.js
@@ -3,10 +3,22 @@ import { tagName } from '@ember-decorators/component';
 import { computed, action } from '@ember-decorators/object';
 import { inject as service } from '@ember-decorators/service';
 
+const SPACEBAR = 32;
+
 @tagName('')
 export default class InteractiveLayerComponent extends Component {
   @service layerSelection;
   @service layerState;
+
+  @action
+  handleKeyDown(e) {
+    if (e.keyCode !== SPACEBAR) {
+      return;
+    }
+
+    const { layerSelection, layer } = this;
+    layerSelection.selectLayer(layer);
+  }
 
   @action
   selectLayer(e) {
