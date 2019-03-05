78baf6a8816ba78e0f471a11423ff1d3c57ead89
Spencer Price <spencer516@gmail.com>
2019-03-04 19:35:58 -0500

    [4. INTERACTIVE LAYER SELECTION]
    fix aria-label-spelling

--- app/components/interactive-layer.js
+++ app/components/interactive-layer.js
@@ -1,5 +1,20 @@
 import Component from '@ember/component';
 import { tagName } from '@ember-decorators/component';
+import { computed, action } from '@ember-decorators/object';
+import { inject as service } from '@ember-decorators/service';
 
 @tagName('')
-export default class InteractiveLayerComponent extends Component {}
+export default class InteractiveLayerComponent extends Component {
+  @service layerSelection;
+  @service layerState;
+
+  @action
+  selectLayer(e) {
+    this.layerSelection.selectLayer(this.layer, e.metaKey || e.shiftKey);
+  }
+
+  @computed('layerSelection.selectedLayers', 'layer')
+  get isSelected() {
+    return this.layerSelection.selectedLayers.includes(this.layer);
+  }
+}
