af2a0b835ffb3648e91a3f2ee48d899ec7393254
Spencer Price <spencer516@gmail.com>
2019-03-04 20:07:27 -0500

    [14. ANNOUNCE LAYER CHANGES]

--- app/services/layer-state.js
+++ app/services/layer-state.js
@@ -1,8 +1,11 @@
 import Service from '@ember/service';
 import { setProperties, set } from '@ember/object';
-import { Sprite } from '../utils/sprites';
+import { Sprite, getDifferences } from '../utils/sprites';
+import { inject as service } from '@ember-decorators/service';
 
 export default class LayerStateService extends Service {
+  @service ariaAnnouncer;
+
   layers = [];
 
   initializeLayers(layers) {
@@ -16,11 +19,27 @@ export default class LayerStateService extends Service {
   }
 
   updateLayer(layer, updatedAttrs) {
+    if (updatedAttrs.sprite) {
+      const { label } = layer;
+      const differences = getDifferences(layer.sprite, updatedAttrs.sprite);
+
+      if (differences.length) {
+        this.ariaAnnouncer.announce('moved_layer', { label });
+      }
+
+      for (const difference of differences) {
+        this.ariaAnnouncer.announce('change_dimension', difference);
+      }
+    }
+
     setProperties(layer, updatedAttrs);
   }
 
   newLayer(layer) {
     this.initializeLayers([...this.layers, layer]);
+
+    this.ariaAnnouncer.announce('layer_created');
+    this.ariaAnnouncer.announce('layer_position', layer.sprite.dimensions);
 
     return layer;
   }
@@ -28,10 +47,15 @@ export default class LayerStateService extends Service {
   deleteLayer(toDelete) {
     const kept = this.layers.filter(layer => layer !== toDelete);
     this.initializeLayers(kept);
+
+    const { label } = toDelete;
+    this.ariaAnnouncer.announce('deleted_layer', { label });
   }
 
   deleteLayers(toDelete) {
     const kept = this.layers.filter(layer => !toDelete.includes(layer));
     this.initializeLayers(kept);
+
+    this.ariaAnnouncer.announce('deleted_layers', { count: toDelete.length });
   }
 }
