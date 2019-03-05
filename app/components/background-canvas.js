647c88f6be1fc95bda621eb778d18880a2c053f7
Spencer Price <spencer516@gmail.com>
2019-03-04 19:49:27 -0500

    [7. RENDERING BACKGROUND SELECTION]

--- app/components/background-canvas.js
+++ app/components/background-canvas.js
@@ -1,8 +1,27 @@
 import Component from '@ember/component';
 import { tagName } from '@ember-decorators/component';
+import { computed } from '@ember-decorators/object';
 
 @tagName('')
 export default class BackgroundCanvasComponent extends Component {
+  isDrawingBox = true;
+  startPosition = [100, 100];
+  currentOffset = [200, 200];
+
   height;
   width;
+
+  @computed('{startPosition,currentOffset}')
+  get boxStyles() {
+    const [x, y] = this.startPosition;
+    const [width, height] = this.currentOffset;
+
+    return {
+      position: 'absolute',
+      left: `${x}px`,
+      top: `${y}px`,
+      width: `${width}px`,
+      height: `${height}px`
+    };
+  }
 }
