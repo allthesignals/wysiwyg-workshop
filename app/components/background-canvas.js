1767f9a6021b9400bf16561305665cd01d1bc24b
Spencer Price <spencer516@gmail.com>
2019-03-04 19:55:44 -0500

    [9. MAKING SELECTION FROM BACKGROUND]

--- app/components/background-canvas.js
+++ app/components/background-canvas.js
@@ -1,7 +1,9 @@
 import Component from '@ember/component';
 import { tagName } from '@ember-decorators/component';
 import { set } from '@ember/object';
+import { inject as service } from '@ember-decorators/service';
 import { computed, action } from '@ember-decorators/object';
+import { Sprite } from '../utils/sprites';
 
 @tagName('')
 export default class BackgroundCanvasComponent extends Component {
@@ -12,8 +14,21 @@ export default class BackgroundCanvasComponent extends Component {
   height;
   width;
 
+  @service layerSelection;
+  @service layerState;
+
+  generateSelectionSprite(offsetParent, dimensions) {
+    const parentBox = offsetParent.getBoundingClientRect();
+    const [startX, startY] = this.startPosition;
+    const [w, h] = dimensions;
+    const x = startX - parentBox.width / 2 + this.width / 2;
+    const y = startY - parentBox.height / 2 + this.height / 2;
+
+    return Sprite.fromDimensions({ x, y, w, h });
+  }
+
   @action
-  backgroundDrag({ event, offset }) {
+  backgroundDrag({ event, offset, element }) {
     const { type } = event;
 
     if (type === 'mousedown') {
@@ -30,6 +45,10 @@ export default class BackgroundCanvasComponent extends Component {
     }
 
     set(this, 'isDrawingBox', false);
+
+    const drawnSprite = this.generateSelectionSprite(element, offset);
+    const intersectingLayers = this.layerState.filterBySprite(drawnSprite);
+    this.layerSelection.selectLayers(intersectingLayers);
   }
 
   @computed('{startPosition,currentOffset}')
