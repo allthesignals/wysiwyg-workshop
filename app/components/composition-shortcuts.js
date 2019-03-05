a3ea7605d0489e1232b3a88f47c09287e35c2a01
Spencer Price <spencer516@gmail.com>
2019-03-04 20:04:03 -0500

    [12. NUDGE LAYERS SHORTCUT]
    # Conflicts:
    #	app/components/composition-shortcuts.js
    #	app/templates/components/composition-shortcuts.hbs

--- app/components/composition-shortcuts.js
+++ app/components/composition-shortcuts.js
@@ -2,11 +2,33 @@ import Component from '@ember/component';
 import { tagName } from '@ember-decorators/component';
 import { inject as service } from '@ember-decorators/service';
 import { action } from '@ember-decorators/object';
+import { Vector } from '../utils/sprites';
 
 @tagName('')
 export default class CompositionShortcutsComponent extends Component {
   @service layerState;
   @service layerSelection;
+
+  @action
+  selectAllTags() {
+    const layers = this.layerState.layers;
+    this.layerSelection.selectLayers(layers);
+  }
+
+  @action
+  deselectAllTags() {
+    this.layerSelection.clearSelection();
+  }
+
+  @action
+  nudge(dx, dy) {
+    const delta = new Vector([dx, dy]);
+
+    for (const layer of this.layerSelection.selectedLayers) {
+      const sprite = layer.sprite.translate(delta);
+      this.layerState.updateLayer(layer, { sprite });
+    }
+  }
 
   @action
   deleteSelected() {
